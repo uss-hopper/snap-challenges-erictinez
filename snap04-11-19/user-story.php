b6c55147346c4ca7e2df54c6c7bc1bb2a58b72d0
Eric Martinez <emartinez451@cnm.edu>
2019-04-11 08:28:41 -0600

    snap challenge 04-11-19

---
+++ snap04-11-19/user-story.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Title</title>
+	</head>
+	<body>
+		<h1>Why is he using our site:</h1>
+		<p>As a new user, I want to upload, add and edit images.</p>
+
+		<a href="persona.php">page 1</a>
+		<a href="use-case.php">page 3</a>
+
+	</body>
+</html>
