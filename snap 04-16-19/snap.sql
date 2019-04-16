18430b10c360d3238f6a910d22dfe7bf5ea4d246
Eric Martinez <emartinez451@cnm.edu>
2019-04-16 08:53:57 -0600

    snap challenge 04-16-19

---
+++ snap 04-16-19/snap.sql
@@ -0,0 +1,12 @@
+drop table if exists task;
+
+create table Task (
+	taskId BINARY(20) not null,
+   taskTitle varchar(255) not null,
+   taskStarDate datetime null,
+   taskDueDate datetime null,
+   taskStatus varchar(64) not null,
+   taskPriority varchar(64) not null,
+   taskDescription varchar(256) null,
+   primary key (taskId)
+);
