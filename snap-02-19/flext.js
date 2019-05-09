ae51f9b68cb37dbda7862f0e8afe532e1fde835f
Eric Martinez <emartinez451@cnm.edu>
2019-05-09 08:51:30 -0600

    i love snap challenges(insert sarcasm here)

---
+++ snap-02-19/flext.js
@@ -0,0 +1,43 @@
+ul {
+	padding: 10rem;
+	margin: 10rem;
+	list-style: none;
+}
+
+li {
+	background: tomato;
+
+	line-height: 200px;
+	color: white;
+	font-weight: bold;
+	font-size: 3em;
+	text-align: center;
+}
+
+<!--correct answer-->
+// .my-list{
+// 	padding: 0;
+// 	margin: 0;
+// 	list-style: none;
+//
+// 	display: flex;
+//
+// 	flex-flow: row wrap;
+// 	justify-content: space-around;
+// }
+//
+// .my-item {
+// 	background: tomato;
+//
+// 	line-height:150px;
+// 	color: white;
+// 	font-weight: bold;
+// 	font-size: 3em;
+// 	text-align: center;
+//
+// 	width: 200px;
+// 	height: 150px;
+// 	padding: 5px;
+// 	margin-top: 10px;
+// }
+//
